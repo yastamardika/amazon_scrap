fa88b121e230b34f6ca1fd0bf012dadb88d57a10
yastamardika <rattyasta1@gmail.com>
2022-03-04 04:30:32 +0700

    add config and seeder

---
+++ seeder/seeder.rb
@@ -0,0 +1,18 @@
+pages << {
+    page_type: "listings",
+    method: "GET",
+    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    url: "https://www.amazon.com/s?k=LED+%26+LCD+TVs&i=electronics&rh=n%3A6459737011&qid=1629141285&ref=sr_pg_1",
+    vars: {
+      category: "LED & LCD TVs"
+    }
+  }
+  pages << {
+    page_type: "listings",
+    method: "GET",
+    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    url: "https://www.amazon.com/s?k=OLED&rh=n%3A6463520011&ref=nb_sb_noss",
+    vars: {
+      category: "OLED TVs"
+    }
+  }
